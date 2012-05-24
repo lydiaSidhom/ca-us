3c9b66eff30d5b9595f40d49559505fb7c9235c2
lydia.sidhom@gmail.com <lydia.sidhom@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-24 23:23:45 +0000

    DataMemory: Read and Write methods ControlUnit

--- Engine/ControlUnit.java
+++ Engine/ControlUnit.java
@@ -1,23 +1,297 @@
 package Engine;
 
 public class ControlUnit {
-	boolean RegDst;
-	boolean Branch;
-	boolean MemRead;
-	boolean MemtoReg;
-	boolean ALUOp;
-	boolean MemWrite;
-	boolean ALUSrc;
-	boolean RegWrite;
-	public ControlUnit(String state) {
-		RegDst=state.charAt(0)=='1'?true:false;
-		Branch=state.charAt(1)=='1'?true:false;
-		MemRead=state.charAt(2)=='1'?true:false;
-		MemtoReg=state.charAt(3)=='1'?true:false;
-		ALUOp=state.charAt(4)=='1'?true:false;
-		MemWrite=state.charAt(5)=='1'?true:false;
-		ALUSrc=state.charAt(6)=='1'?true:false;
-		RegWrite=state.charAt(7)=='1'?true:false;
+	int RegDst0;
+	int RegDst1;
+	int Branch;
+	int MemRead;
+	int MemtoReg0;
+	int MemtoReg1;
+	int ALUOp0;
+	int ALUOp1;
+	int ALUOp2;
+	int MemWrite;
+	int ALUSrc;
+	int RegWrite;
+	int Jump0;
+	int Jump1;
 
+	public ControlUnit() {
+	}
+
+	public void setControls(String opcode, String function) {
+
+		if (opcode.equals("000000") && function.equals("001000")) {
+			//jr instruction
+			RegDst0 = -1;
+			RegDst1 = -1;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = -1;
+			MemtoReg1 = -1;
+			ALUOp0 = -1;
+			ALUOp1 = -1;
+			ALUOp2 = -1;
+			MemWrite = 0;
+			ALUSrc = -1;
+			RegWrite = 0;
+			Jump0 = 0;
+			Jump1 = 1;
+		} else if (opcode.equals("000000")) {
+			//other R-format instruction
+			RegDst0 = 1;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 0;
+			ALUOp0 = 0;
+			ALUOp1 = 0;
+			ALUOp2 = 0;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("000010")) {
+			//jump instruction
+			RegDst0 = -1;
+			RegDst1 = -1;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = -1;
+			MemtoReg1 = -1;
+			ALUOp0 = -1;
+			ALUOp1 = -1;
+			ALUOp2 = -1;
+			MemWrite = 0;
+			ALUSrc = -1;
+			RegWrite = 0;
+			Jump0 = 1;
+			Jump1 = 0;
+		}
+		if (opcode.equals("000011")) {
+			//jump and link instruction
+			RegDst0 = 0;
+			RegDst1 = 1;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 1;
+			ALUOp0 = -1;
+			ALUOp1 = -1;
+			ALUOp2 = -1;
+			MemWrite = 0;
+			ALUSrc = -1;
+			RegWrite = 1;
+			Jump0 = 1;
+			Jump1 = 0;
+		}
+		if (opcode.equals("000100")) {
+			//beq
+			RegDst0 = -1;
+			RegDst1 = -1;
+			Branch = 1;
+			MemRead = 0;
+			MemtoReg0 = -1;
+			MemtoReg1 = -1;
+			ALUOp0 = 1;
+			ALUOp1 = 0;
+			ALUOp2 = 0;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 0;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("000101")) {
+			//bne
+			RegDst0 = -1;
+			RegDst1 = -1;
+			Branch = 1;
+			MemRead = 0;
+			MemtoReg0 = -1;
+			MemtoReg1 = -1;
+			ALUOp0 = 1;
+			ALUOp1 = 0;
+			ALUOp2 = 0;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 0;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("001000")) {
+			//addi
+			RegDst0 = 0;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 0;
+			ALUOp0 = 0;
+			ALUOp1 = 0;
+			ALUOp2 = 1;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("001010")) {
+			//slti
+			RegDst0 = 0;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 0;
+			ALUOp0 = 0;
+			ALUOp1 = 1;
+			ALUOp2 = 0;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("001011")) {
+			//sltui
+			RegDst0 = 0;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 0;
+			ALUOp0 = 1;
+			ALUOp1 = 1;
+			ALUOp2 = 0;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("001100")) {
+			//andi
+			RegDst0 = 0;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 0;
+			ALUOp0 = 1;
+			ALUOp1 = 0;
+			ALUOp2 = 1;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("001101")) {
+			//ori
+			RegDst0 = 0;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = 0;
+			MemtoReg1 = 0;
+			ALUOp0 = 0;
+			ALUOp1 = 1;
+			ALUOp2 = 1;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("100011")) {
+			//lw
+			RegDst0 = 0;
+			RegDst1 = 0;
+			Branch = 0;
+			MemRead = 1;
+			MemtoReg0 = 1;
+			MemtoReg1 = 0;
+			ALUOp0 = 1;
+			ALUOp1 = 1;
+			ALUOp2 = 1;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+		if (opcode.equals("101011")) {
+			//sw
+			RegDst0 = -1;
+			RegDst1 = -1;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg0 = -1;
+			MemtoReg1 = -1;
+			ALUOp0 = 1;
+			ALUOp1 = 1;
+			ALUOp2 = 1;
+			MemWrite = 1;
+			ALUSrc = 1;
+			RegWrite = 0;
+			Jump0 = 0;
+			Jump1 = 0;
+		}
+	}
+
+	public int getRegDst0() {
+		return RegDst0;
+	}
+
+	public int getRegDst1() {
+		return RegDst1;
+	}
+
+	public int getBranch() {
+		return Branch;
+	}
+
+	public int getMemRead() {
+		return MemRead;
+	}
+
+	public int getMemtoReg0() {
+		return MemtoReg0;
+	}
+
+	public int getMemtoReg1() {
+		return MemtoReg1;
+	}
+
+	public int getALUOp0() {
+		return ALUOp0;
+	}
+
+	public int getALUOp1() {
+		return ALUOp1;
+	}
+
+	public int getMemWrite() {
+		return MemWrite;
+	}
+
+	public int getALUSrc() {
+		return ALUSrc;
+	}
+
+	public int getRegWrite() {
+		return RegWrite;
+	}
+
+	public int getJump0() {
+		return Jump0;
+	}
+
+	public int getJump1() {
+		return Jump1;
 	}
 }
