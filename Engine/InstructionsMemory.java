563e114ea3e65cfb266e84ecf8654d7e0c40d44d
christinesed@gmail.com <christinesed@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-24 18:37:22 +0000

    Commited InstructionsMemory

--- Engine/InstructionsMemory.java
+++ Engine/InstructionsMemory.java
@@ -1,6 +1,34 @@
 package Engine;
 
+/*
+Author : Christine.
+This is the class of InstructionsMemory.... it takes two inputs the beginning address
+of instructions (i.e. origin (for example org 32))
+and list of binary instructions computed before...
+
+Provides getInstructionAt(that takes a certain address and returns the instruction at this address
+from the list of binary instructions.)
+
+*/
+
+import java.util.ArrayList;
 public class InstructionsMemory {
-	String readAddress;
+	int beginAddress;
+	ArrayList<String> binaryInstructions=new ArrayList<String>();
 	
+	public InstructionsMemory(int origin, ArrayList<String> instructions) {
+		this.beginAddress=origin;
+		this.binaryInstructions=instructions;
+	}
+
+	public String getInstructionAt(int address) {
+		try {
+
+			return this.binaryInstructions.get(address-beginAddress);
+		
+		} catch(Exception e) {
+			System.out.println("Trying to Accessing wrong address No instruction to fetch.");
+			return "ERROR";
+		}
+	}
 }
