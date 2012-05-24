5cdf0cbfba6433169d87d213d67dcdb05439d78e
lydia.sidhom@gmail.com <lydia.sidhom@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-24 23:23:45 +0000

    DataMemory: Read and Write methods ControlUnit

--- trunk/Engine/DataMemory.java
+++ trunk/Engine/DataMemory.java
@@ -1,5 +1,32 @@
 package Engine;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class DataMemory {
+	
+	
+	Map<String, String> myMemory = new HashMap<String, String>();
 
+	public String readMemory(String address) {
+		try {
+			String value = myMemory.get(address);
+			return value;
+		
+		} catch(Exception e) {
+			System.out.println("Trying to Accessing wrong address No value ");
+			return "Error";
+		}
+	}
+	
+	
+	public void writeMemory(String address , String data) {
+		try {
+				myMemory.put (address , data);
+				
+		} 
+		catch(Exception e) {
+			System.out.println("Trying to Accessing wrong address");
+		}
+	}
 }
