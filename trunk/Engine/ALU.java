96b327b9a2ee6b6bad6c9d354a0d560ec82f3d57
mina.workspace@gmail.com <mina.workspace@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-25 02:37:36 +0000

    Update ALU to Be Signed

--- trunk/Engine/ALU.java
+++ trunk/Engine/ALU.java
@@ -1,5 +1,7 @@
 package Engine;
 
+import java.math.BigInteger;
+
 public class ALU {
 	String readInput1;
 	String readInput2;
@@ -7,58 +9,111 @@ public class ALU {
 	String ALUResult;
 	int operation;
 	boolean ALUZero;
-	
-	static String AND 			= "0000";
-	static String OR 			= "0001";
-	static String ADD 			= "0010";
-	static String SUBTRACT 		= "0110";
+
+	static String AND = "0000";
+	static String OR = "0001";
+	static String ADD = "0010";
+	static String SUBTRACT = "0110";
 	static String SETONLESSTHAN = "0111";
-	static String NOR 			= "1100";
+	static String NOR = "1100";
 
 	public ALU() {
-		
+
 	}
 
 	public void doOperation() throws Exception {
-		if(readInput1==null||readInput2==null||readInput1.length()<32 || readInput2.length()<32){
+		if (readInput1 == null || readInput2 == null
+				|| readInput1.length() < 32 || readInput2.length() < 32) {
 			throw new Exception("Wrong Input !!");
 		}
-		switch(operation){
-		case 0 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) + Integer.parseInt(readInput2,2));break;
-		case 1 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) | Integer.parseInt(readInput2,2));break;
-		case 2 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) & Integer.parseInt(readInput2,2));break;
-		case 3 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) - Integer.parseInt(readInput2,2));break;
-		case 4 : ALUResult = Integer.toBinaryString((Integer.parseInt(readInput1,2) < Integer.parseInt(readInput2,2))?1:0);break;
-		case 5 : ALUResult = Integer.toBinaryString(~((Integer.parseInt(readInput1,2) | Integer.parseInt(readInput2,2))));break;
-		default : throw new Exception("Operation Failure !!");
+		switch (operation) {
+		case 0:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					+ Integer.parseInt(readInput2, 2));
+			break;
+		case 1:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					| Integer.parseInt(readInput2, 2));
+			break;
+		case 2:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					& Integer.parseInt(readInput2, 2));
+			break;
+		case 3:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					- Integer.parseInt(readInput2, 2));
+			break;
+		case 4:
+			ALUResult = Integer
+					.toBinaryString((Integer.parseInt(readInput1, 2) < Integer
+							.parseInt(readInput2, 2)) ? 1 : 0);
+			break;
+		case 5:
+			ALUResult = Integer
+					.toBinaryString((Integer.parseInt(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) < Integer
+							.parseInt(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)) ? 1 : 0);
+			break;
+		case 6:
+			ALUResult = Integer.toBinaryString(~((Integer.parseInt(readInput1,
+					2) | Integer.parseInt(readInput2, 2))));
+			break;
+		default:
+			throw new Exception("Operation Failure !!");
 		}
-		if(ALUResult.length()>32){
+		if (ALUResult.length() > 32) {
 			ALUResult.substring(1);
 		}
-		if(Integer.parseInt(ALUResult,2) == 0){
-			ALUZero =true;
-		}else{
+		if (Integer.parseInt(ALUResult, 2) == 0) {
+			ALUZero = true;
+		} else {
 			ALUZero = false;
 		}
 	}
-	
-	public void doOperation(String readInput1,String readInput2,int operation) throws Exception {
-		if(readInput1==null||readInput2==null||readInput1.length()<32 || readInput2.length()<32){
+
+	public void doOperation(String readInput1, String readInput2, int operation)
+			throws Exception {
+		if (readInput1 == null || readInput2 == null
+				|| readInput1.length() < 32 || readInput2.length() < 32) {
 			throw new Exception("Wrong Input !!");
 		}
-		switch(operation){
-		case 0 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) + Integer.parseInt(readInput2,2));break;
-		case 1 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) | Integer.parseInt(readInput2,2));break;
-		case 2 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) & Integer.parseInt(readInput2,2));break;
-		case 3 : ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1,2) - Integer.parseInt(readInput2,2));break;
-		case 4 : ALUResult = Integer.toBinaryString((Integer.parseInt(readInput1,2) < Integer.parseInt(readInput2,2))?1:0);break;
-		case 5 : ALUResult = Integer.toBinaryString(~((Integer.parseInt(readInput1,2) | Integer.parseInt(readInput2,2))));break;
-		default : throw new Exception("ERROR !!!");
+		switch (operation) {
+		case 0:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					+ Integer.parseInt(readInput2, 2));
+			break;
+		case 1:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					| Integer.parseInt(readInput2, 2));
+			break;
+		case 2:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					& Integer.parseInt(readInput2, 2));
+			break;
+		case 3:
+			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
+					- Integer.parseInt(readInput2, 2));
+			break;
+		case 4:
+			ALUResult = Integer
+					.toBinaryString((Integer.parseInt(readInput1, 2) < Integer
+							.parseInt(readInput2, 2)) ? 1 : 0);
+			break;
+		case 5:
+			ALUResult = Integer
+					.toBinaryString((Integer.parseInt(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) < Integer
+							.parseInt(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)) ? 1 : 0);
+			break;
+		case 6:
+			ALUResult = Integer.toBinaryString(~((Integer.parseInt(readInput1,
+					2) | Integer.parseInt(readInput2, 2))));
+			break;
+		default:
+			throw new Exception("ERROR !!!");
 		}
-		if(ALUResult.length()>32){
+		if (ALUResult.length() > 32) {
 			ALUResult.substring(1);
 		}
-		
+
 	}
 
 	public void setReadInput1(String input) {
@@ -68,31 +123,50 @@ public class ALU {
 	public void setReadInput2(String input) {
 		readInput2 = input;
 	}
-	
+
 	public void setALUFunction(String input) {
 		ALUFunction = input;
-		if(ALUFunction.equals(AND)){
+		if (ALUFunction.equals(AND)) {
 			operation = 0;
 		}
-		if(ALUFunction.equals(OR)){
+		if (ALUFunction.equals(OR)) {
 			operation = 1;
 		}
-		if(ALUFunction.equals(ADD)){
+		if (ALUFunction.equals(ADD)) {
 			operation = 2;
 		}
-		if(ALUFunction.equals(SUBTRACT)){
+		if (ALUFunction.equals(SUBTRACT)) {
 			operation = 3;
 		}
-		if(ALUFunction.equals(SETONLESSTHAN)){
+		if (ALUFunction.equals(SETONLESSTHAN)) {
 			operation = 4;
 		}
-		if(ALUFunction.equals(NOR)){
+		if (ALUFunction.equals(NOR)) {
 			operation = 5;
 		}
-		
+
+	}
+
+	public static String twoComplement(String s) {
+		boolean found = false;
+		String temp = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (found)
+				temp = s.charAt(i) == '1' ? 0 + temp : '1' + temp;
+			else if (s.charAt(i) == '1') {
+				found = true;
+				temp = 1 + temp;
+			} else
+				temp = 0 + temp;
+		}
+		return temp;
 	}
 
 	public String getALUResult() {
 		return ALUResult;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(twoComplement("11111111111"));
 	}
 }
