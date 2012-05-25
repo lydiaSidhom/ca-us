ff028fc8c197416f055c8895e0a029a1b471e823
mina.workspace@gmail.com <mina.workspace@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-25 13:54:15 +0000

    Added new ALU + Its Control

--- Engine/ALU.java
+++ Engine/ALU.java
@@ -1,7 +1,5 @@
 package Engine;
 
-import java.math.BigInteger;
-
 public class ALU {
 	String readInput1;
 	String readInput2;
@@ -15,7 +13,10 @@ public class ALU {
 	static String ADD = "0010";
 	static String SUBTRACT = "0110";
 	static String SETONLESSTHAN = "0111";
+	static String SETONLESSTHANUNSIGNED = "1000";
 	static String NOR = "1100";
+	static String SLL = "1010";
+	static String SRL = "1001";
 
 	public ALU() {
 
@@ -28,42 +29,57 @@ public class ALU {
 		}
 		switch (operation) {
 		case 0:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					+ Integer.parseInt(readInput2, 2));
+			ALUResult = Long.toBinaryString(Long.parseLong(readInput1, 2)
+					& Long.parseLong(readInput2, 2));
 			break;
 		case 1:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					| Integer.parseInt(readInput2, 2));
+			ALUResult = Long.toBinaryString(Long.parseLong(readInput1, 2)
+					| Long.parseLong(readInput2, 2));
 			break;
 		case 2:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					& Integer.parseInt(readInput2, 2));
+			ALUResult = Long
+			.toBinaryString((Long.parseLong(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) + Long
+					.parseLong(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)));
 			break;
 		case 3:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					- Integer.parseInt(readInput2, 2));
+			ALUResult = Long
+			.toBinaryString((Long.parseLong(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) - Long
+					.parseLong(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)));
 			break;
 		case 4:
-			ALUResult = Integer
-					.toBinaryString((Integer.parseInt(readInput1, 2) < Integer
-							.parseInt(readInput2, 2)) ? 1 : 0);
+			ALUResult = Long
+					.toBinaryString((Long.parseLong(readInput1, 2) < Long
+							.parseLong(readInput2, 2)) ? 1 : 0);
 			break;
 		case 5:
-			ALUResult = Integer
-					.toBinaryString((Integer.parseInt(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) < Integer
-							.parseInt(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)) ? 1 : 0);
+			ALUResult = Long
+					.toBinaryString((Long.parseLong(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) < Long
+							.parseLong(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)) ? 1 : 0);
 			break;
 		case 6:
-			ALUResult = Integer.toBinaryString(~((Integer.parseInt(readInput1,
-					2) | Integer.parseInt(readInput2, 2))));
+			ALUResult = Long.toBinaryString(~((Long.parseLong(readInput1,
+					2) | Long.parseLong(readInput2, 2))));
+			break;
+		case 7:
+			ALUResult = Long.toBinaryString(((Long.parseLong(readInput1,
+					2) << Long.parseLong(readInput2, 2))));
+			break;
+		case 8:
+			ALUResult = Long.toBinaryString(((Long.parseLong(readInput1,
+					2) >> Long.parseLong(readInput2, 2))));
 			break;
 		default:
-			throw new Exception("Operation Failure !!");
+			System.out.print("ALU Idle");
 		}
 		if (ALUResult.length() > 32) {
-			ALUResult.substring(1);
+			ALUResult = ALUResult.substring(ALUResult.length()-32);
 		}
-		if (Integer.parseInt(ALUResult, 2) == 0) {
+		if (ALUResult.length() < 32) {
+			for(int i =ALUResult.length();i<32;i++){
+				ALUResult = 0+ALUResult;
+			}
+		}
+		if (Long.parseLong(ALUResult, 2) == 0) {
 			ALUZero = true;
 		} else {
 			ALUZero = false;
@@ -78,37 +94,45 @@ public class ALU {
 		}
 		switch (operation) {
 		case 0:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					+ Integer.parseInt(readInput2, 2));
+			ALUResult = Long.toBinaryString(Long.parseLong(readInput1, 2)
+					+ Long.parseLong(readInput2, 2));
 			break;
 		case 1:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					| Integer.parseInt(readInput2, 2));
+			ALUResult = Long.toBinaryString(Long.parseLong(readInput1, 2)
+					| Long.parseLong(readInput2, 2));
 			break;
 		case 2:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					& Integer.parseInt(readInput2, 2));
+			ALUResult = Long.toBinaryString(Long.parseLong(readInput1, 2)
+					& Long.parseLong(readInput2, 2));
 			break;
 		case 3:
-			ALUResult = Integer.toBinaryString(Integer.parseInt(readInput1, 2)
-					- Integer.parseInt(readInput2, 2));
+			ALUResult = Long.toBinaryString(Long.parseLong(readInput1, 2)
+					- Long.parseLong(readInput2, 2));
 			break;
 		case 4:
-			ALUResult = Integer
-					.toBinaryString((Integer.parseInt(readInput1, 2) < Integer
-							.parseInt(readInput2, 2)) ? 1 : 0);
+			ALUResult = Long
+					.toBinaryString((Long.parseLong(readInput1, 2) < Long
+							.parseLong(readInput2, 2)) ? 1 : 0);
 			break;
 		case 5:
-			ALUResult = Integer
-					.toBinaryString((Integer.parseInt(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) < Integer
-							.parseInt(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)) ? 1 : 0);
+			ALUResult = Long
+					.toBinaryString((Long.parseLong(readInput1.charAt(0)=='1'?"-"+twoComplement(readInput1):readInput1, 2) < Long
+							.parseLong(readInput2.charAt(0)=='1'?"-"+twoComplement(readInput2):readInput2, 2)) ? 1 : 0);
 			break;
 		case 6:
-			ALUResult = Integer.toBinaryString(~((Integer.parseInt(readInput1,
-					2) | Integer.parseInt(readInput2, 2))));
+			ALUResult = Long.toBinaryString(~((Long.parseLong(readInput1,
+					2) | Long.parseLong(readInput2, 2))));
+			break;
+		case 7:
+			ALUResult = Long.toBinaryString(((Long.parseLong(readInput1,
+					2) << Long.parseLong(readInput2, 2))));
+			break;
+		case 8:
+			ALUResult = Long.toBinaryString(((Long.parseLong(readInput1,
+					2) >> Long.parseLong(readInput2, 2))));
 			break;
 		default:
-			throw new Exception("ERROR !!!");
+			System.out.print("ALU Idle");
 		}
 		if (ALUResult.length() > 32) {
 			ALUResult.substring(1);
@@ -116,35 +140,59 @@ public class ALU {
 
 	}
 
-	public void setReadInput1(String input) {
+	public void setReadInput1(String input) throws Exception{
+		if (input.length()!=32)
+			throw new Exception("Error Wrong Input");
 		readInput1 = input;
 	}
 
-	public void setReadInput2(String input) {
+	public void setReadInput2(String input) throws Exception{
+		if (input.length()!=32)
+			throw new Exception("Error Wrong Input");
 		readInput2 = input;
 	}
 
-	public void setALUFunction(String input) {
+	public void setALUFunction(String input) throws Exception{
+		if (input.length()!=4)
+			throw new Exception("Error Wrong Input");
 		ALUFunction = input;
 		if (ALUFunction.equals(AND)) {
 			operation = 0;
+			return;
 		}
 		if (ALUFunction.equals(OR)) {
 			operation = 1;
+			return;
 		}
 		if (ALUFunction.equals(ADD)) {
 			operation = 2;
+			return;
 		}
 		if (ALUFunction.equals(SUBTRACT)) {
 			operation = 3;
+			return;
 		}
 		if (ALUFunction.equals(SETONLESSTHAN)) {
 			operation = 4;
+			return;
+		}
+		if (ALUFunction.equals(SETONLESSTHANUNSIGNED)) {
+			operation = 5;
+			return;
 		}
 		if (ALUFunction.equals(NOR)) {
-			operation = 5;
+			operation = 6;
+			return;
 		}
-
+		if (ALUFunction.equals(SLL)) {
+			operation = 7;
+			return;
+		}
+		if (ALUFunction.equals(SRL)) {
+			operation = 8;
+			return;
+		}
+		throw new Exception("Error in Alu Function");
 	}
 
 	public static String twoComplement(String s) {
@@ -167,6 +215,16 @@ public class ALU {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(twoComplement("11111111111"));
+		ALU TEST = new ALU();
+		TEST.operation = 8;
+		TEST.readInput1 = "11111111111111111111111111101110";
+		TEST.readInput2 = "00000000000000000000000000000011";
+		try {
+			TEST.doOperation();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(TEST.ALUResult);
 	}
 }
--- Engine/ALUControlUnit.java
+++ Engine/ALUControlUnit.java
@@ -1,5 +1,165 @@
 package Engine;
 
 public class ALUControlUnit {
-
+	
+	String ALUOpcode;
+	String funct;
+	String ResultALUFunction;
+	
+	static String RFORMAT 	= "000";
+	static String BEQN 		= "001";
+	static String SLTI 		= "010";
+	static String SLTIU 	= "011";
+	static String ADDI 		= "100";
+	static String ANDI 		= "101";
+	static String ORI 		= "110";
+	static String LWSW 		= "111";
+	
+	static String ADDCODE 	= "010000";
+	static String SUBCODE 	= "100010";
+	static String SLLCODE 	= "000000";
+	static String SRLCODE	= "000010";
+	static String ANDCODE 	= "100100";
+	static String ORCODE	= "100101";
+	static String NORCODE	= "100111";
+	static String SLTCODE	= "101010";
+	static String SLTUCODE	= "101011";
+	
+	int ALUOpcodeId;
+	int functId;
+	
+	public ALUControlUnit(){
+	}
+	
+	public void generateALUFunction(){
+		switch(ALUOpcodeId){
+		case 0:{
+			switch(functId){
+			case 0:	ResultALUFunction = "0010";break;
+			case 1: ResultALUFunction = "0110";break;
+			case 2: ResultALUFunction = "1010";break;
+			case 3: ResultALUFunction = "1001";break;
+			case 4: ResultALUFunction = "0000";break;
+			case 5: ResultALUFunction = "0001";break;
+			case 6: ResultALUFunction = "1100";break;
+			case 7: ResultALUFunction = "0111";break;
+			case 8: ResultALUFunction = "1000";break;
+			default: System.out.println("ALUControl Idle");
+			}
+		}break;
+		case 1:ResultALUFunction = "0110";break;
+		case 2:ResultALUFunction = "0111";break;
+		case 3:ResultALUFunction = "1000";break;
+		case 4:ResultALUFunction = "0010";break;
+		case 5:ResultALUFunction = "0000";break;
+		case 6:ResultALUFunction = "0001";break;
+		case 7:ResultALUFunction = "0010";break;
+		}
+	}
+	
+	public void setALUopcode(String input) throws Exception{
+		if(input.length()==3)
+		ALUOpcode = input;
+		else
+		throw new Exception("Wrong Input to ALUOpcode !");
+		if(input.equals(RFORMAT)){
+			ALUOpcodeId = 0;
+			return;
+		}
+		if(input.equals(BEQN)){
+			ALUOpcodeId = 1;
+			return;
+		}
+		if(input.equals(SLTI)){
+			ALUOpcodeId = 2;
+			return;
+		}
+		if(input.equals(SLTIU)){
+			ALUOpcodeId = 3;
+			return;
+		}
+		if(input.equals(ADDI)){
+			ALUOpcodeId = 4;
+			return;
+		}
+		if(input.equals(ANDI)){
+			ALUOpcodeId = 5;
+			return;
+		}
+		if(input.equals(ORI)){
+			ALUOpcodeId = 6;
+			return;
+		}
+		if(input.equals(LWSW)){
+			ALUOpcodeId = 7;
+			return;
+		}
+		throw new Exception("Wrong Input to ALUOpcode !");
+	}
+	
+	public void setFunct(String input) throws Exception{
+		if(input.length()==6)
+			funct = input;
+			else
+			throw new Exception("Wrong Input to funct !");
+			if(input.equals(ADDCODE)){
+				functId = 0;
+				return;
+			}
+			if(input.equals(SUBCODE)){
+				functId = 1;
+				return;
+			}
+			if(input.equals(SLLCODE)){
+				functId = 2;
+				return;
+			}
+			if(input.equals(SRLCODE)){
+				functId = 3;
+				return;
+			}
+			if(input.equals(ANDCODE)){
+				functId = 4;
+				return;
+			}
+			if(input.equals(ORCODE)){
+				functId = 5;
+				return;
+			}
+			if(input.equals(NORCODE)){
+				functId = 6;
+				return;
+			}
+			if(input.equals(SLTCODE)){
+				functId = 7;
+				return;
+			}
+			if(input.equals(SLTUCODE)){
+				functId = 8;
+				return;
+			}
+			throw new Exception("Wrong Input to funct !");
+	}
+	
+	public String getResultALUFuncion(){
+		return ResultALUFunction;
+	}
+	
+	public static void main(String [] args){
+		ALUControlUnit TEST = new ALUControlUnit();
+		try {
+			TEST.setALUopcode("000");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			TEST.setFunct("000010");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		TEST.generateALUFunction();
+		System.out.println(TEST.getResultALUFuncion());
+	}
 }
