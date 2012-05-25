916d8ef93e9b4557f6613e1b378379e0e6b2b613
mina.workspace@gmail.com <mina.workspace@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-25 15:08:56 +0000

    Adding Register

---
+++ Engine/Register.java
@@ -0,0 +1,21 @@
+package Engine;
+
+public class Register {
+	String name;
+	String value;
+	public Register(String name){
+		this.name = name;
+		this.value = "00000000000000000000000000000000";
+	}
+	
+	public void setValue(String input) throws Exception{
+		if(input.length() ==32){
+			this.value = input;
+		}else{
+			throw new Exception("Wron input into register");
+		}
+	}
+	public String getValue(){
+		return  this.value;
+	}
+}
