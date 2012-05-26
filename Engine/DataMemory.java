cf50084cbeb2b184f9bf3ad7a7011a2ecf1be0d7
mina.workspace@gmail.com <mina.workspace@gmail.com@a479ba2f-a664-e695-094c-553c4bfd1f2e>
2012-05-26 19:11:49 +0000

    Fixing DataMemory Class to be like a hardware component.

--- Engine/DataMemory.java
+++ Engine/DataMemory.java
@@ -7,26 +7,52 @@ public class DataMemory {
 	
 	
 	Map<String, String> myMemory = new HashMap<String, String>();
-
-	public String readMemory(String address) {
-		try {
-			String value = myMemory.get(address);
-			return value;
+	String inputAddress;
+	String inputData;
+	String outData;
+	boolean writeSignal;
+	boolean readSignal;
+	
+	public DataMemory(){
 		
-		} catch(Exception e) {
-			System.out.println("Trying to Accessing wrong address No value ");
-			return "Error";
-		}
 	}
 	
-	
-	public void writeMemory(String address , String data) {
-		try {
-				myMemory.put (address , data);
-				
-		} 
-		catch(Exception e) {
-			System.out.println("Trying to Accessing wrong address");
+	public void setData(String input)throws Exception{
+		if(input.length() != 32)
+			throw new Exception("Wrong Input");
+			this.inputData = input;
+		}
+	public void setWriteAddress(String input)throws Exception{
+		if(input.length() != 32)
+			throw new Exception("Wrong Input");
+			this.inputAddress = input;
+		}
+	public void setWritingSignal(boolean input){
+		writeSignal = input;
+	}
+	public void setReadingSignal(boolean input){
+		readSignal = input;
+	}
+	public void doDataMemoryAction(){
+		if(writeSignal){
+			myMemory.put (inputAddress , inputData);
+			return;
+		}
+		if(readSignal){
+			outData = myMemory.get (inputAddress);
+			if(outData == null)
+				outData = "00000000000000000000000000000000"; 
+			return;
 		}
 	}
+	public String getOutputData(){
+		return outData;
+	}
+	public static void main(String [] args){
+		DataMemory TEST = new DataMemory();
+		TEST.myMemory.put("000000000000000000000000000000","11111111111111111111111111111111");
+		String value =TEST.myMemory.get("000000000000000000000000000000");
+		System.out.println(value);
+	}
+	
 }
